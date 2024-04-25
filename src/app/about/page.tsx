72ca42b2e85500969c9e4ba4cd343f5b64c00607
Raxuis <raclot.raphael@gmail.com>
2024-04-25 15:20:18 +0200

    feat: :sparkles: Disable buttons when already on page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
 import { Layout } from '@/components/layout'
-import { buttonVariants } from '@/components/ui/ui/button'
+import { buttonVariants } from '@/components/ui/button'
 import { Header } from '@/layout/Header'
 import { Link } from 'next-view-transitions'
 import React from 'react'
 
 const About = () => {
+  const isAboutPage = true;
+
   return (
     <Layout>
       <Header />
@@ -15,12 +17,19 @@ const About = () => {
             size: 'lg',
           }
         )} href='/'>Go to Home</Link>
-        <Link className={buttonVariants(
-          {
-            variant: 'outline',
-            size: 'lg',
-          }
-        )} href='/about'>Go to About</Link>
+        <Link
+          className={buttonVariants(
+            {
+              variant: 'default',
+              size: 'lg'
+            }
+          )}
+          href='/about'
+          aria-disabled={isAboutPage ? 'true' : 'false'}
+          style={{ pointerEvents: isAboutPage ? 'none' : 'auto', opacity: isAboutPage ? 0.5 : 1 }}
+        >
+          Already in About
+        </Link>
       </div>
     </Layout>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Layout } from '@/components/layout'
-import { buttonVariants } from '@/components/ui/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 import { Header } from '@/layout/Header';
 import { Link } from 'next-view-transitions'
 
 
 export default async function Home() {
+
+  const isAboutPage = true;
 
   return (
     <Layout>
@@ -16,13 +18,20 @@ export default async function Home() {
             size: 'lg',
           }
         )} href='/about'>Go to About</Link>
-        <Link className={buttonVariants(
-          {
-            variant: 'outline',
-            size: 'lg',
-          }
-        )} href='/about'>Go to Home</Link>
+        <Link
+          className={buttonVariants(
+            {
+              variant: 'default',
+              size: 'lg'
+            }
+          )}
+          href='/'
+          aria-disabled={isAboutPage ? 'true' : 'false'}
+          style={{ pointerEvents: isAboutPage ? 'none' : 'auto', opacity: isAboutPage ? 0.5 : 1 }}
+        >
+          Already in Home
+        </Link>
       </div>
     </Layout>
-  );
+  )
 }

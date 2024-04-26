be212a7d88cadb79a1d786c58e0afd5d52e8f8bf
Raxuis <raclot.raphael@gmail.com>
2024-04-26 11:29:35 +0200

    feat(bento): :art: Add bento grid to home page

--- src/components/pages-components/Bento.tsx
+++ src/components/pages-components/Bento.tsx
@@ -4,19 +4,22 @@ import { motion, MotionProps } from 'framer-motion';
 import { twMerge } from 'tailwind-merge';
 import Image from 'next/image';
 import { buttonVariants } from '../ui/button';
-import { ArrowTopRightIcon, TwitterLogoIcon } from '@radix-ui/react-icons';
+import { ArrowTopRightIcon } from '@radix-ui/react-icons';
+import { FaGithub, FaStackOverflow, FaTwitter, FaYoutube } from "react-icons/fa";
 import { Link } from 'next-view-transitions';
+import ReactCountryFlag from "react-country-flag"
+
 const Bento = () => {
   return (
-    <div className='min-h-screen py-12'>
-      <div className='mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4'>
+    <div className='min-h-screen w-full py-12'>
+      <motion.div initial="initial" animate="animate" transition={{
+        staggerChildren: 0.1
+      }} className='mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4'>
         <HeaderBlock />
-        <SocialBlock />
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-      </div>
+        <SocialsBlock />
+        <AboutBlock />
+        <LocationBlock />
+      </motion.div>
     </div>
   )
 }
@@ -29,16 +32,34 @@ type Props = {
 
 const Block = ({ className, ...rest }: Props) => {
   return <motion.div
+    variants={{
+      initial: {
+        scale: 0.5,
+        y: -50,
+        opacity: 0,
+      },
+      animate: {
+        scale: 1,
+        y: 0,
+        opacity: 1
+      },
+    }}
+    transition={{
+      type: 'spring',
+      stiffness: 400,
+      damping: 50,
+      mass: 3
+    }}
     className={twMerge("col-span-4 rounded-lg border border-zinc-700 bg-zinc-800 p-6", className)} {...rest} />
 }
 
 const HeaderBlock = () => {
-  return <Block className='col-span-12 row-span-2 max-md:col-span-6'>
+  return <Block className='col-span-12 row-span-2 md:col-span-6'>
     <Image src='/edited-raph.jpg' alt='raphael-avatar' priority={false} width={50} height={50} className='mb-4 size-14 rounded-full object-cover' />
     <h1 className='mb-12 text-2xl font-medium leading-tight'>
       Hi, I&apos;m Raphaël | Raxuis. {" "}
       <span className="text-zinc-400">
-        A French junior Front End Developer 🇫🇷
+        A junior Front End Developer
       </span>
     </h1>
     <Link href='https://raphaelraclot.vercel.app/' className={buttonVariants(
@@ -50,14 +71,65 @@ const HeaderBlock = () => {
   </Block>
 }
 
-const SocialBlock = () => {
+const SocialsBlock = () => {
   return (
     <>
-      <Block className='col-span-6 bg-card md:col-span-3'>
+      <Block whileHover={{
+        rotate: '-2.5deg',
+        scale: 1.05
+      }} className='col-span-6 bg-blue-500 md:col-span-3'>
         <Link href="https://www.x.com/i_haruki_i" className='grid h-full place-content-center text-3xl text-white'>
-          <TwitterLogoIcon />
+          <FaTwitter />
+        </Link>
+      </Block>
+      <Block whileHover={{
+        rotate: '2.5deg',
+        scale: 1.05
+      }} className='col-span-6 bg-card md:col-span-3'>
+        <Link href="https://github.com/raxuis" className='grid h-full place-content-center text-3xl text-white'>
+          <FaGithub />
+        </Link>
+      </Block>
+      <Block whileHover={{
+        rotate: '2.5deg',
+        scale: 1.05
+      }} className='col-span-6 bg-orange-500 md:col-span-3'>
+        <Link href="https://stackoverflow.com/users/22539453/haruki" className='grid h-full place-content-center text-3xl text-white'>
+          <FaStackOverflow />
+        </Link>
+      </Block>
+      <Block whileHover={{
+        rotate: '-2.5deg',
+        scale: 1.05
+      }} className='col-span-6 bg-red-500 md:col-span-3'>
+        <Link href="https://www.youtube.com/channel/UCni1RGwtSZXMZUzHeKukSjw" className='grid h-full place-content-center text-3xl text-white'>
+          <FaYoutube />
         </Link>
       </Block>
     </>
   );
+}
+const AboutBlock = () => {
+  return (
+    <Block className='col-span-12 text-2xl leading-snug'>
+      <p>
+        My passions are Basketball, Formula 1, coding and cybersecurity. {" "}
+        <span className='text-zinc-400'>
+          I mainly code in React and Tailwind CSS. However, I&apos;m really curious so I&apos;ve already tried Prisma, NextJS, Svelte, PHP, Vanilla JavaScript, SCSS, Python, C, MySQL, Bash. Finally, for React, I use TypeScript for my code&apos;s safety.
+        </span>
+      </p>
+    </Block>
+  )
+};
+
+const LocationBlock = () => {
+  return (
+    <Block className='col-span-12 flex flex-col items-center gap-4 md:col-span-3'>
+      <ReactCountryFlag countryCode="FR" svg style={{
+        width: '1.5em',
+        height: '1.5em',
+      }} />
+      <p className='text-xl text-zinc-400'>France</p>
+    </Block>
+  )
 }

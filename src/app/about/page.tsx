51bfcbef17551d3106e3703c90eb17c0847a1ca3
Raxuis <raclot.raphael@gmail.com>
2024-04-27 18:58:46 +0200

    refactor(about): :recycle: Remove useless imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,6 @@
 import { Layout } from '@/components/layout'
 import Bento from '@/components/pages-components/Bento'
-import { buttonVariants } from '@/components/ui/button'
 import { Header } from '@/layout/Header'
-import { Link } from 'next-view-transitions'
 import React from 'react'
 
 const About = () => {

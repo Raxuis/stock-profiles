b27257621e20ea9aaab83ce4bfbe96f5e634e94b
Raxuis <raclot.raphael@gmail.com>
2024-06-23 20:15:20 +0200

    feat(prisma): :card_file_box: initial migration of prisma schema

---
+++ prisma/migrations/20240623181410_initial_migration/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "QueryType" AS ENUM ('StockChart', 'StockProfile');
+
+-- CreateEnum
+CREATE TYPE "Plan" AS ENUM ('FREE', 'PRO');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "plan" "Plan" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Query" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" "QueryType" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Query_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Query" ADD CONSTRAINT "Query_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

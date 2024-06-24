95978ae8fa06707cd4d5d6ff9d797460325405c5
Raxuis <raclot.raphael@gmail.com>
2024-06-24 19:00:18 +0200

    feat(account): :sparkles: add max user queries check

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,6 +4,8 @@ import { prisma } from "@/prisma";
 
 export default async function Home() {
   const user = await currentUser();
+
+  const MAXUSERQUERIES = 100;
   if (!user) {
     return <p className="text-center text-3xl">Please sign in</p>;
   }
@@ -27,8 +29,13 @@ export default async function Home() {
     <>
       <p className="text-center text-3xl">Welcome {user!.name}!</p>
       <p>You've done {userQueriesCount} {userQueriesCount <= 1 ? "query" : "queries"}!</p>
+      {userQueriesCount >= MAXUSERQUERIES && (
+        <p className="text-center text-sm">
+          You have reached the limit of {MAXUSERQUERIES} queries. Please subscribe to our PREMIUM plan.
+        </p>
+      )}
       <Progress value={
-        userQueriesCount
+        MAXUSERQUERIES
       } max={100} />
       <ul>
         {userQueries.map((query) => (

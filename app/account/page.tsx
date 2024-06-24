5576bb542db093f43388fd6d6366512a09ae511f
Raxuis <raclot.raphael@gmail.com>
2024-06-24 15:11:16 +0200

    feat(account): :sparkles: add verification if user is logged

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,33 +1,11 @@
 import { currentUser } from "@/auth/current-user";
-import { buttonVariants } from "@/components/ui/button";
-import { Link } from "next-view-transitions";
 
 export default async function Home() {
   const user = await currentUser()
-  if (user) {
-    console.log(user)
-    // const welcomeMessage = user?.firstName || user?.username;
-    return (
-      <p>welcome</p>
-      // <p className="pt-2 text-center text-3xl">Welcome {welcomeMessage != null ? welcomeMessage : 'User'}!</p>
-    )
+  if (!user) {
+    return <p className="text-center text-3xl">Please sign in</p>
   }
   return (
-    <div className="flex flex-col items-center space-y-3">
-      <p className="pt-2 text-3xl">Not logged</p>
-      <p className="text-xl">Please log in</p>
-      <div className="flex space-x-5">
-        <Link href="/sign-in" className={buttonVariants(
-          {
-            variant: "outline"
-          }
-        )} >Sign In</Link>
-        <Link href="/sign-up" className={buttonVariants(
-          {
-            variant: "outline"
-          }
-        )} >Sign Up</Link>
-      </div>
-    </div>
+    <p className="text-center text-3xl">Welcome {user!.name}!</p>
   )
 }

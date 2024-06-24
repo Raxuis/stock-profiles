9b10d55845777c6380099d3b0147d7d4068b8ce0
Raxuis <raclot.raphael@gmail.com>
2024-06-24 15:05:09 +0200

    feat(header): :sparkles: add avatar in the header if user is logged

--- src/features/auth/LoggedInButton.tsx
+++ src/features/auth/LoggedInButton.tsx
@@ -20,42 +20,49 @@ export const LoggedInButton = async () => {
     )
   }
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="focus:outline-none">
-        <RxHamburgerMenu size={20} />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuItem asChild>
-          <Link href="/" className="w-full">
-            <Home size={16} className="mr-2" />
-            Home
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/account" className="w-full">
-            <User size={16} className="mr-2" />
-            Account
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/stock-profile" className="w-full">
-            <Building2 size={16} className="mr-2" />
-            Stock Profile
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/stock-chart" className="w-full">
-            <Activity size={16} className="mr-2" />
-            Stock Chart
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <Link href="/about" className="w-full">
-            <Fingerprint size={16} className="mr-2" />
-            About
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex gap-2">
+      {user.image && (
+        <div className="size-7 rounded-full">
+          <img src={user.image} className="rounded-full" />
+        </div>
+      )}
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <RxHamburgerMenu size={20} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem asChild>
+            <Link href="/" className="w-full">
+              <Home size={16} className="mr-2" />
+              Home
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/account" className="w-full">
+              <User size={16} className="mr-2" />
+              Account
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/stock-profile" className="w-full">
+              <Building2 size={16} className="mr-2" />
+              Stock Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/stock-chart" className="w-full">
+              <Activity size={16} className="mr-2" />
+              Stock Chart
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/about" className="w-full">
+              <Fingerprint size={16} className="mr-2" />
+              About
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   )
 }

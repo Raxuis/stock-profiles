979eea9c80dd372d8de9ac162eaf5851ac58379d
Raxuis <raclot.raphael@gmail.com>
2024-08-26 11:25:36 +0200

    feat(header banner): :dizzy: add framer motion animations

---
+++ src/components/Banner.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from 'react'
+import Image from "next/image"
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const Banner = () => {
+  return (
+    <>
+      <Link href="/" className="group flex flex-1 items-center gap-2">
+        <Image src="/icon.png" priority={true} width={30} height={30} alt="stocks-profiles-logo" className="duration-500 group-hover:scale-105" />
+        <motion.p
+          className="text-lg"
+          animate={
+            {
+              opacity: 1,
+              x: 0,
+            }
+          }
+          initial={
+            {
+              opacity: 0,
+              x: -20,
+            }
+          }
+          transition={
+            {
+              duration: 0.5,
+              ease: "easeInOut",
+            }
+          }>Stocks Profiles</motion.p>
+      </Link>
+    </>
+  )
+}
+
+export default Banner
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image"
+import Banner from "@/components/Banner";
 import { Layout } from "@/components/layout";
-import Link from "next/link";
 import { LoggedInButton } from "@/features/auth/LoggedInButton";
 
 
@@ -8,10 +7,7 @@ export const Header = () => {
   return (
     <header className="w-full border-b border-border">
       <Layout className="flex flex-row items-center">
-        <Link href="/" className="group flex flex-1 items-center gap-2">
-          <Image src="/icon.png" priority={true} width={30} height={30} alt="stocks-profiles-logo" className="duration-500 group-hover:scale-105" />
-          <p className="text-lg">Stocks Profiles</p>
-        </Link>
+        <Banner />
         <LoggedInButton />
       </Layout>
     </header>

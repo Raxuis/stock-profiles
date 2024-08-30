14823d64cb06ab26574bf55acacf3c824ff959de
Raxuis <raclot.raphael@gmail.com>
2024-08-30 18:55:57 +0200

    feat(homepage): :sparkles: add cursor function to improve UI

---
+++ src/components/cursor/CursorFunction.tsx
@@ -0,0 +1,61 @@
+import { SVGProps } from 'react';
+import { Cursor } from '@/components/core/Cursor';
+
+const MouseIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width={26}
+      height={31}
+      fill='none'
+      {...props}
+    >
+      <g clipPath='url(#a)'>
+        <path
+          fill={'#f24a3b'}
+          fillRule='evenodd'
+          stroke={'#fff'}
+          strokeLinecap='square'
+          strokeWidth={2}
+          d='M21.993 14.425 2.549 2.935l4.444 23.108 4.653-10.002z'
+          clipRule='evenodd'
+        />
+      </g>
+      <defs>
+        <clipPath id='a'>
+          <path fill={'#f24a3b'} d='M0 0h26v31H0z' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export function Cursor2({ children, text }: { children: React.ReactNode, text: string }) {
+  return (
+    <div className='py-12'>
+      <div className='overflow-hidden'>
+        <Cursor
+          attachToParent
+          variants={{
+            initial: { scale: 0.3, opacity: 0 },
+            animate: { scale: 1, opacity: 1 },
+            exit: { scale: 0.3, opacity: 0 },
+          }}
+          transition={{
+            ease: 'easeInOut',
+            duration: 0.15,
+          }}
+          className='left-12 top-4'
+        >
+          <div>
+            <MouseIcon className='size-6' />
+            <div className='ml-4 mt-1 rounded-[4px] bg-destructive px-2 py-0.5 text-destructive-foreground'>
+              {text}
+            </div>
+          </div>
+        </Cursor>
+        {children}
+      </div>
+    </div>
+  );
+}
--- src/components/pages-components/home/LandingPage.tsx
+++ src/components/pages-components/home/LandingPage.tsx
@@ -5,14 +5,17 @@ import LandingSection from '@/components/pages-components/home/LandingSection';
 import { BorderBeam } from '@/components/magicui/border-beam';
 import Image from 'next/image';
 import CompaniesStack from '@/components/CompaniesStack';
+import { Cursor2 } from '@/components/cursor/CursorFunction';
 
 export default function LandingPage(props) {
   return (
     <>
       <LandingSection />
       <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-        <img src="/stocks-profiles.png" alt="Hero Image" className='size-full object-contain' />
-        <BorderBeam size={250} duration={12} delay={9} />
+        <Cursor2 text="Website screenshot">
+          <img src="/stocks-profiles.png" alt="Hero Image" className='size-full object-contain' />
+          <BorderBeam size={250} duration={12} delay={9} />
+        </Cursor2>
       </div>
       <ProblemsSection />
       <CompaniesStack />

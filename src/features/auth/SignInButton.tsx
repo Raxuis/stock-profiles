94b8d212df0b4f471f571850b828b7ff79bcdbc5
Raxuis <raclot.raphael@gmail.com>
2024-08-31 15:46:11 +0200

    feat(homepage): :lipstick: add a custom button to log in

--- src/features/auth/SignInButton.tsx
+++ src/features/auth/SignInButton.tsx
@@ -1,13 +1,20 @@
 "use client";
+import ShinyButton from "@/components/magicui/shiny-button";
 import { buttonVariants } from "@/components/ui/button"
 import { LogIn } from "lucide-react";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export const SignInButton = () => {
   return <form>
-    <Link href="/api/auth/signin"
-      className={buttonVariants({ variant: "secondary", size: "sm" })}>
+    <ShinyButton
+      href="/api/auth/signin"
+      className={cn(
+        buttonVariants({ variant: "secondary", size: "sm" }),
+        "bg-primary/80 text-primary-foreground duration-500 hover:bg-primary/90",
+      )} >
       <LogIn size={16} className="mr-2" />
-      Sign In</Link>
+      Sign In
+    </ShinyButton>
   </form>
 }

2e89b012cd3f6354ebaecbdcf8b8f38e817e749e
Raxuis <raclot.raphael@gmail.com>
2024-09-02 17:28:54 +0200

    style(favorite or not star): :dizzy: add framer motion animations on tap and hover of stars

--- src/components/FavoriteOrNotStar.tsx
+++ src/components/FavoriteOrNotStar.tsx
@@ -1,12 +1,35 @@
 "use client";
 
-import { useEffect } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa6";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 const FavoriteOrNotStar = ({ symbol, userId, isFavorite }: { symbol: string; userId: string; isFavorite: boolean }) => {
   const router = useRouter();
+
+  const variants = {
+    initial: { scale: 0.9 },
+    animate: { scale: 1.0 },
+    whileTap: {
+      scale: 0.9,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 5,
+        mass: 0.5,
+      }
+    },
+    whileHover: {
+      scale: 1.1,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 5,
+        mass: 0.5,
+      }
+    },
+  };
 
   const handleFavoriteClick = async () => {
     await axios.post('/api/toggle-favorite', { symbol, userId });
@@ -14,9 +37,9 @@ const FavoriteOrNotStar = ({ symbol, userId, isFavorite }: { symbol: string; use
   };
 
   return (
-    <p onClick={handleFavoriteClick} className="cursor-pointer">
+    <motion.p onClick={handleFavoriteClick} className="cursor-pointer" variants={variants} initial="initial" animate="animate" whileTap="whileTap" whileHover="whileHover">
       {isFavorite ? <FaStar className="text-yellow-500" /> : <FaRegStar className="text-yellow-500" />}
-    </p>
+    </motion.p>
   );
 };
 

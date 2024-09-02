ca49de7858bb662a44ff62c0104df933c200ad34
Raxuis <raclot.raphael@gmail.com>
2024-09-02 14:39:03 +0200

    feat(contact page): :sparkles: add description of the page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,14 +4,22 @@ import { AnimatedListDemo } from '@/components/notifications/AnimatedListDemo'
 
 const Contact = () => {
   return (
-    <div className='mt-10 grid h-[500px] grid-cols-12 place-items-center gap-4'>
-      <div className='col-span-6 flex flex-col justify-center'>
-        <div className='size-full overflow-hidden'>
-          <AnimatedListDemo />
-        </div>
+    <div className='mt-10 flex flex-col space-y-10'>
+      <div className='flex flex-col space-y-2'>
+        <h1 className='text-2xl font-bold'>Contact</h1>
+        <p className='text-lg'>
+          Have a question or want to work together?
+        </p>
       </div>
-      <div className='col-span-6 flex size-full items-center justify-center'>
-        <ContactForm />
+      <div className='grid h-[500px] grid-cols-12 place-items-center gap-4'>
+        <div className='col-span-6 flex flex-col justify-center'>
+          <div className='size-full overflow-hidden'>
+            <AnimatedListDemo />
+          </div>
+        </div>
+        <div className='col-span-6 flex size-full items-center justify-center'>
+          <ContactForm />
+        </div>
       </div>
     </div>
   )

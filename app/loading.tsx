0504c79bfe61bdbacffd7639e0ac7d35e459e3ca
Raxuis <raclot.raphael@gmail.com>
2024-09-02 16:50:44 +0200

    style(loading): :lipstick: improve loading UI

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,11 @@
+import Spinner from "@/components/animata/progress/spinner";
 import { Layout } from "@/components/layout";
 import { Loader2 } from "lucide-react";
 export default function Loading() {
   return (
     <Layout>
-      <div className="flex gap-2 self-center pt-10">
-        <Loader2 className="h-6 animate-spin" />
-        <p>Loading...</p>
+      <div className="flex self-center pt-10">
+        <Spinner className="flex items-center justify-center" />
       </div>
     </Layout>
   )
--- src/components/magicui/shiny-button.tsx
+++ src/components/magicui/shiny-button.tsx
@@ -40,7 +40,11 @@ const ShinyButton = ({
     <motion.button
       {...animationProps}
       onClick={() => {
-        router.push(href);
+        if (href) {
+          router.push(href); // Ensure href is valid
+        } else {
+          console.error("Href is not provided or invalid");
+        }
       }}
       className={cn(
         "relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-[box-shadow] duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",

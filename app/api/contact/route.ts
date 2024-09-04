ee0c95dd367ca175a43900848d850a5e3ef44f22
Raxuis <raclot.raphael@gmail.com>
2024-09-04 14:00:59 +0200

    feat(emails): :sparkles: add a check if user already sent an email

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,7 +3,17 @@ import { type NextRequest, NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 
+const emailRateLimitMap = new Map<string, { count: number; timestamp: number }>();
+
 export async function POST(request: NextRequest) {
+  const ip = request.ip || request.headers.get('x-forwarded-for') || 'localhost';
+  const rateLimit = emailRateLimitMap.get(ip) || { count: 0, timestamp: Date.now() };
+
+  // Check if the user exceeded the email limit (e.g., 1 email per minute)
+  if (rateLimit.count >= 1 && Date.now() - rateLimit.timestamp < 60 * 1000) {
+    return NextResponse.json({ error: 'Too many requests. Please wait a minute before sending another email.' }, { status: 429 });
+  }
+
   const { email, name, message } = await request.json();
 
   const transport = nodemailer.createTransport({
@@ -17,7 +27,6 @@ export async function POST(request: NextRequest) {
   const mailOptions: Mail.Options = {
     from: email,
     to: env.EMAIL_USER,
-    // cc: email, (uncomment this line if you want to send a copy to the sender)
     subject: `Message from ${name} (${email})`,
     text: message,
   };
@@ -35,6 +44,12 @@ export async function POST(request: NextRequest) {
 
   try {
     await sendMailPromise();
+
+    // Update rate limit data
+    rateLimit.count++;
+    rateLimit.timestamp = Date.now();
+    emailRateLimitMap.set(ip, rateLimit);
+
     return NextResponse.json({ message: 'Email sent' });
   } catch (err) {
     return NextResponse.json({ error: err }, { status: 500 });

2824d89534975a52862ba2e425b27a9f72a3cdab
Raxuis <raclot.raphael@gmail.com>
2024-09-07 13:52:49 +0200

    fix(contact page): :iphone: fix issue on responsive with missing props

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,28 +1,28 @@
-import ContactForm from '@/components/ContactForm'
-import React from 'react'
-import { AnimatedListDemo } from '@/components/notifications/AnimatedListDemo'
+import ContactForm from '@/components/ContactForm';
+import React from 'react';
+import { AnimatedListDemo } from '@/components/notifications/AnimatedListDemo';
 
 const Contact = () => {
   return (
-    <div className='mt-10 flex flex-col space-y-10'>
-      <div className='flex flex-col space-y-2'>
-        <h1 className='text-2xl font-bold'>Contact</h1>
-        <p className='text-lg'>
+    <div className="mt-10 flex flex-col space-y-10">
+      <div className="flex flex-col space-y-2">
+        <h1 className="text-2xl font-bold">Contact</h1>
+        <p className="text-lg">
           Have a question or want to work together?
         </p>
       </div>
-      <div className='grid h-[500px] grid-cols-12 place-items-center gap-4'>
-        <div className='col-span-6 flex flex-col justify-center'>
-          <div className='size-full overflow-hidden'>
+      <div className="grid h-auto grid-cols-1 gap-4 sm:grid-cols-2 sm:flex-col">
+        <div className="order-2 flex flex-col justify-center sm:order-none">
+          <div className="size-full">
             <AnimatedListDemo />
           </div>
         </div>
-        <div className='col-span-6 flex size-full items-center justify-center'>
+        <div className="order-1 flex size-full items-center justify-center sm:order-none">
           <ContactForm />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

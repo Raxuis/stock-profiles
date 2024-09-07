3c825a21f3400e2b270372ebb982fea83fa861b1
Raxuis <raclot.raphael@gmail.com>
2024-09-07 18:36:54 +0200

    style(homepage): :lipstick: improve ui for landing page

--- src/components/CompaniesStack.tsx
+++ src/components/CompaniesStack.tsx
@@ -3,8 +3,8 @@ import { companies } from "@/constants/availableCompanies";
 
 export default function CompaniesStack() {
   return (
-    <div className="mb-8 flex flex-col items-center justify-center space-y-14 sm:space-y-28">
-      <p className="text-2xl font-bold">Stocks we provide</p>
+    <div className="mb-8 flex flex-col items-center justify-center space-y-14 sm:space-y-16">
+      <p className="text-4xl font-bold">Stocks we provide</p>
       <div className="mx-auto max-w-5xl px-8">
         <HoverEffect items={companies} />
       </div>

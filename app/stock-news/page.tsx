ed9a0673c49eb19b15c956a2cc7757748988916a
Raxuis <raclot.raphael@gmail.com>
2024-09-09 12:41:23 +0200

    feat(Stock News): :sparkles: add stock news query

--- app/stock-news/page.tsx
+++ app/stock-news/page.tsx
@@ -6,21 +6,26 @@ import { z } from 'zod';
 import { FormNewsSchema } from '@/lib/validation';
 import { Input } from '@/components/ui/input';
 import { getStockNews } from '@/features/stocks/stock.action';
+import { useState } from 'react';
+import { StockNewsType } from '@/types/StockNews.type';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 
 const StockNews = () => {
+
+  const [news, setNews] = useState<StockNewsType[]>([]);
+
   const form = useZodForm({
     schema: FormNewsSchema,
     defaultValues: {
-      symbol: 'AAPL',
-      number: 5,
+      symbol: 'AAPL'
     },
   });
 
   const onSubmit = async (data: z.infer<typeof FormNewsSchema>) => {
     try {
       const response = await getStockNews(data.symbol, data.number);
-      console.log(response);
+      setNews(response.stories);
       toast({
         title: "📈 Wow 📈",
         description: (
@@ -38,39 +43,61 @@ const StockNews = () => {
   };
 
   return (
-    <Form form={form} onSubmit={onSubmit}>
-      <FormField
-        name="symbol"
-        render={({ field }) => (
+    <>
+      <Form form={form} onSubmit={onSubmit}>
+        <FormField
+          name="symbol"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Stock&apos;s Symbol</FormLabel>
+              <FormControl>
+                <Input
+                  autoFocus
+                  placeholder="Symbol"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage>{form.formState.errors.symbol?.message}</FormMessage>
+            </FormItem>
+          )}
+        />
+        <FormField name="number" render={({ field }) => (
           <FormItem>
-            <FormLabel>Stock&apos;s Symbol</FormLabel>
+            <FormLabel>Number of news</FormLabel>
             <FormControl>
               <Input
                 autoFocus
-                placeholder="Symbol"
+                placeholder="Number"
+                type='number'
                 {...field}
               />
             </FormControl>
-            <FormMessage>{form.formState.errors.symbol?.message}</FormMessage>
+            <FormMessage>{form.formState.errors.number?.message}</FormMessage>
           </FormItem>
-        )}
-      />
-      <FormField name="number" render={({ field }) => (
-        <FormItem>
-          <FormLabel>Number of news</FormLabel>
-          <FormControl>
-            <Input
-              autoFocus
-              placeholder="Number"
-              type='number'
-              {...field}
-            />
-          </FormControl>
-          <FormMessage>{form.formState.errors.number?.message}</FormMessage>
-        </FormItem>
-      )} />
-      <Button type="submit" className='mt-3 max-sm:w-full'>Search</Button>
-    </Form>
+        )} />
+        <Button type="submit" className='mt-3 max-sm:w-full'>Search</Button>
+      </Form>
+      {news.length > 0 && (
+        <div className='mt-8 flex justify-center'>
+          {news.map((news: StockNewsType) => (
+            <Card key={news.url} className='w-full sm:w-2/3 md:w-1/2'>
+              <CardHeader className='flex items-center px-10 py-6'>
+                {news.favicon_url && <img src={news.favicon_url} alt={news.title} className='size-20' />}
+                <div className='flex flex-col space-y-1.5 p-6'>
+                  {news.title && <CardTitle className='flex items-center'> {news.title}</CardTitle>}
+                  {news.description && <CardDescription>Queried: {news.description}</CardDescription>}
+                </div>
+              </CardHeader>
+              <CardContent className='flex flex-col space-y-6 '>
+                <div className='space-y-4 px-2'>
+                  Test
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      )}
+    </>
   )
 }
 
---
+++ src/types/StockNews.type.ts
@@ -0,0 +1,11 @@
+export type StockNewsType = {
+  description: string;
+  favicon_url: string;
+  id: string;
+  site: string;
+  tags: string[];
+  tickers: string[];
+  time: Number;
+  title: string;
+  url: string;
+}
